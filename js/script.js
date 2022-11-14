a083fe555d0895179a013063db42ecb58ab0d51c
Claire-Bedrossian <Claire-Bedrossian@users.noreply.github.com>
2022-11-14 19:46:52 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -18,14 +18,14 @@ if (navigator.serviceWorker) {
 /**
  * This function updates the slider value.
  */
- function updateSliderValue(valueFromSlider) {
+function updateSliderValue(valueFromSlider) {
   document.getElementById("slider-value").innerHTML = valueFromSlider
 }
 
 /**
  * This function displays the slider value.
  */
- function myButtonClicked() {
+function myButtonClicked() {
   document.getElementById("hello-world").innerHTML =
     "<p>Value is: " + slider.value + "</p>"
 }
@@ -33,15 +33,13 @@ if (navigator.serviceWorker) {
 /**
  * This function generates a random number.
  */
- randomNum = Math.floor(Math.random() * 10) + 1;  // returns a random integer from 1 to 10
+randomNum = Math.floor(Math.random() * 10) + 1 // returns a random integer from 1 to 10
 
- /**
+/**
  * This function determines if it is equal to the generated number */
 
- if(valueFromSlider == randomNum){
-  "<p> That is the correct answer. </p>"
-  }
-  else{
-    "<p> That is the wrong answer. </p>"
-  }
-
+if (valueFromSlider == randomNum) {
+  ;("<p> That is the correct answer. </p>")
+} else {
+  ;("<p> That is the wrong answer. </p>")
+}
