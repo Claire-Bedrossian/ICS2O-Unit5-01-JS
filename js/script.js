71efdc8b0bd1850ef707a66a6935f6cbe6483cb1
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-14 14:44:54 -0500

    added functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,38 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-01-JS/sw.js", {
+    scope: "/ICS2O-Unit5-01-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function updates the slider value.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
 }
+
+/**
+ * This function displays the slider value.
+ */
+ function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
+}
+
+/**
+ * This function generates a random number.
+ */
+ randomNum = Math.floor(Math.random() * 10) + 1;  // returns a random integer from 1 to 10
+
+ /**
+ * This function determines if it is equal to the generated number */
+
+ if(valueFromSlider == randomNum){
+  "<p> That is the correct answer. </p>"
+  }
+  else{
+    "<p> That is the wrong answer. </p>"
+  }
+
